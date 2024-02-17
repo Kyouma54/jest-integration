dd7d9f62683967e6b38eeeb294df8c32f2bc0ada
Filipe Freitas <f566119@corp.caixa.gov.br>
2024-02-17 06:50:49 -0300

    Primeiro commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup'
+};
